c85b837c1da1170538b05525146ed56fda07ddfb
Steven Quach <stevendquach@gmail.com>
2020-09-18 06:26:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++){
     if (cart[i].itemName == item){
       return cart.splice(i,1)}
-else return (`That item is not in your cart.`)   }    
+      else return (`That item is not in your cart.`)   }    
   // write your code here}
 }
 
