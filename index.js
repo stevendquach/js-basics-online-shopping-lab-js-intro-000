c23616038e3f164b3c8d254aab13d30f474792a4
Steven Quach <stevendquach@gmail.com>
2020-09-18 06:28:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ function removeFromCart(item) {
     if (cart[i].itemName == item){
       return cart.splice(i,1)}
     }
-    return (`That item is not in your cart.`)  
-   }    
+    return (`That item is not in your cart.`)
+   }
   // write your code here}
 }
 
