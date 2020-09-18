11bdedd5f12afa9bdfafc06db1c92d8d8d694c2c
Steven Quach <stevendquach@gmail.com>
2020-09-18 06:41:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ for(let i=0; i < cart.length; i++){
 function placeOrder(cardNumber) {
   var totals = total()
   if (cardNumber == undefined){
-    return (`Sorry, we dont't have a credit card on file for you.`) }
+    return (`Sorry, we don't have a credit card on file for you.`) }
   else  {
     cart = []
     return (`Your total cost is $${totals}, which will be charged to the card ${cardNumber}.`)
