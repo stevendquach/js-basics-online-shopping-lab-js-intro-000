7751cbc83275f0797bdb91399f003cb1c79db335
Steven Quach <stevendquach@gmail.com>
2020-09-18 06:22:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
-  if (cart[i].itemName == item)
+  if (cart[i].itemName == item){
         cart.splice(i,1)
-  else return (`That item is not in your cart.`)   }    
-  // write your code here
+  else return (`That item is not in your cart.`)   }    }
+  // write your code here}
 }
 
 function placeOrder(cardNumber) {
